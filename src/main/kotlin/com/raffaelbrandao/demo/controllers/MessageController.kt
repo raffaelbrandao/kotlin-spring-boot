daf782afb666e3ac80252f3b44d5b46e0e2bb0cc
Rafael Brandão <raffaelbrandao@gmail.com>
2024-02-22 00:50:59 -0300

    Update MessageController class adding message function

--- src/main/kotlin/com/raffaelbrandao/demo/controllers/MessageController.kt
+++ src/main/kotlin/com/raffaelbrandao/demo/controllers/MessageController.kt
@@ -1,5 +1,6 @@
 package com.raffaelbrandao.demo.controllers
 
+import com.raffaelbrandao.demo.data.Message
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -8,4 +9,10 @@ import org.springframework.web.bind.annotation.RestController
 class MessageController {
     @GetMapping("/")
     fun index(@RequestParam("name") name : String) = "Hello, $name!"
+    @GetMapping("/message")
+    fun message() = listOf(
+            Message("1", "Hello!"),
+            Message("2", "Bonjour!"),
+            Message("3", "Privet!"),
+    )
 }
